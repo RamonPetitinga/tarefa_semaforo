f0f682dc41966c036f7bd09e0c834491068d0b3e
RamonPetitinga <ramonlpbahia@gmail.com>
2025-02-04 09:46:16 -0300

    Correção código

--- tarefa_semaforo.c
+++ tarefa_semaforo.c
@@ -3,7 +3,7 @@
 #include "hardware/timer.h" // Inclui a biblioteca para gerenciamento de temporizadores de hardware.
 
 #define led_vermelho 11
-#define led_azul 12
+#define led_amarelo 12
 #define led_verde 13
 bool led_on = false;
 
@@ -13,27 +13,30 @@ int contador = 1;
 // O tipo bool indica que a função deve retornar verdadeiro ou falso para continuar ou parar o temporizador.
 bool repeating_timer_callback_3S(struct repeating_timer *t)
 {
-    // Imprime uma mensagem na saída serial indicando que 1 segundo se passou.
-    // printf("3 segundos passaram\n");
+    // Imprime uma mensagem na saída serial indicando que 3 segundos se passaram.
+    printf("3 segundos passaram\n");
     // Liga ou desliga o led.
     switch (contador)
     {
     case 1:
         gpio_put(led_vermelho, true);
-        gpio_put(led_azul, false);
+        gpio_put(led_amarelo, false);
         gpio_put(led_verde, false);
+        printf("LED vermelho ligado\n");
         contador++;
         break;
     case 2:
         gpio_put(led_vermelho, false);
-        gpio_put(led_azul, true);
+        gpio_put(led_amarelo, true);
         gpio_put(led_verde, false);
+        printf("LED azul ligado\n");
         contador++;
         break;
     case 3:
         gpio_put(led_vermelho, false);
-        gpio_put(led_azul, false);
+        gpio_put(led_amarelo, false);
         gpio_put(led_verde, true);
+        printf("LED verde ligado\n");
         contador = 1;
         break;
     }
@@ -44,39 +47,39 @@ bool repeating_timer_callback_3S(struct repeating_timer *t)
 bool repeating_timer_callback_1S(struct repeating_timer *t)
 {
     // Imprime uma mensagem na saída serial indicando que 1 segundo se passou.
-    // printf("1 segundo se passou\n");
+    printf("1 segundo se passou\n");
     // Retorna true para manter o temporizador repetindo. Se retornar false, o temporizador para.
     return true;
 }
 
 int main()
 {
-
     int tempo = 1;
 
     // Inicializa a comunicação serial, permitindo o uso de funções como printf.
     stdio_init_all();
 
-    // Inicializar o pino GPIO11
+    // Inicializar os pinos GPIO
     gpio_init(led_vermelho);
-    gpio_set_dir(led_vermelho, true);
-    gpio_init(led_azul);
-    gpio_set_dir(led_azul, true);
+    gpio_set_dir(led_vermelho, GPIO_OUT);
+    gpio_init(led_amarelo);
+    gpio_set_dir(led_amarelo, GPIO_OUT);
     gpio_init(led_verde);
-    gpio_set_dir(led_verde, true);
+    gpio_set_dir(led_verde, GPIO_OUT);
 
     // Declaração de uma estrutura de temporizador de repetição.
     // Esta estrutura armazenará informações sobre o temporizador configurado.
     struct repeating_timer timer1, timer2;
 
-    // Configura o temporizador para chamar a função de callback a cada 1 segundo.
+    // Configura o temporizador para chamar a função de callback a cada 3 segundos.
     add_repeating_timer_ms(3000, repeating_timer_callback_3S, NULL, &timer1);
+    // Configura o temporizador para chamar a função de callback a cada 1 segundo.
     add_repeating_timer_ms(1000, repeating_timer_callback_1S, NULL, &timer2);
 
     // Loop infinito que mantém o programa em execução.
     while (true)
     {
-        // Pausa de 10 segundos para reduzir o uso da CPU.
+        // Pausa de 1 segundo para reduzir o uso da CPU.
         sleep_ms(1000);
         printf("Se passou %d segundo(s)\n", tempo);
         tempo++;
